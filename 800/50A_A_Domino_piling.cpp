0d1f6afd32599d03f3036ff45d69f508041f2beb
Md. Shakibul Islam <shakibulislam74254285@gmail.com>
2026-02-05 21:44:44 +0600

    New codefoces solutions

---
+++ 800/50A_A_Domino_piling.cpp
@@ -0,0 +1,14 @@
+// 05/01/2026
+// Problem name: A. Domino piling
+// https://codeforces.com/problemset/problem/50/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    cout << (m*n)/2 << endl;
+    return 0;
+}

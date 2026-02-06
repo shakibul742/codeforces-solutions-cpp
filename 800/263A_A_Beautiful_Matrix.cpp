677a5e2b1f30b5a21e81e157e6671d213e5a3bf4
Md. Shakibul Islam <shakibulislam74254285@gmail.com>
2026-02-06 23:48:32 +0600

    Add daily Codeforces solution for 06-02-2026

---
+++ 800/263A_A_Beautiful_Matrix.cpp
@@ -0,0 +1,31 @@
+// 06-02-2026
+// Problem name: A. Beautiful Matrix
+// https://codeforces.com/problemset/problem/263/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i, j, arr[5][5];
+
+    for(i = 0; i < 5; i++)
+    {
+        for(j = 0; j < 5; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for(i = 0; i < 5; i++)
+    {
+        for(j = 0; j < 5; j++)
+        {
+            if(arr[i][j] == 1)
+            {
+                cout  << abs(i - 2) + abs(j - 2) << endl;
+                return 0;
+            }
+        }
+    }
+}
